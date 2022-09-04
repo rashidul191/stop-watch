ef988060feb343f4846dcbaef3c00285a9492778
rashidul191 <rashidul.191cse.gub@gmail.com>
2022-09-04 17:42:15 +0600

    create stop-watch react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [time, setTime] = useState(0);
+  const [running, setRunning] = useState(false);
+
+  useEffect(() => {
+    let interval;
+    if (running) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 10);
+      }, 10);
+    } else if (!running) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [running]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h3>Stopwatch</h3>
+        <div className="watchCount">
+          <span>{("0" + Math.floor((time / 3600000) % 60)).slice(-2)}:</span>
+          <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+          <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
+          <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
+        </div>
+        <div className="styleBtn">
+          <button className="btn start" onClick={() => setRunning(true)}>
+            Start
+          </button>
+          <button className="btn stop" onClick={() => setRunning(false)}>
+            Stop
+          </button>
+          <button className="btn reset" onClick={() => setTime(0)}>
+            Reset
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
